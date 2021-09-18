735c43cf8f59074c46c1d5bb7e75d11d8b63a472
Hemanth <hemanth.aero@gmail.com>
2021-09-18 18:16:19 +0530

    Impl e2e tunnel for get without hdrs

--- tunnel-client/client.py
+++ tunnel-client/client.py
@@ -1,17 +1,31 @@
 import asyncio
+import aiohttp
 import websockets
 import json
+
+local_svc = "http://localhost:9002"
 
 async def hello():
   async with websockets.connect('ws://localhost:9001') as websocket:
     while True:
       try:
-        msg = await websocket.recv()
-        print("> {}".format(msg))
+        req_msg = await websocket.recv()
+        print("> {}".format(req_msg))
 
-        return_msg = json.loads(msg)
+        # Make the call using aiohttp
+        # https://docs.aiohttp.org/en/stable/client_quickstart.html
+        # TODO Send headers
+        req_msg_json = json.loads(req_msg)
+        if req_msg_json["method"] == "GET":
+          async with aiohttp.ClientSession() as session:
+            async with session.get(local_svc + req_msg_json["uri"]) as resp:
+              print("Response status from svc: " + str(resp.status))
+              resp_body = await resp.text()
+
+        return_msg = req_msg_json
         # TODO Encode body as base64 string
-        return_msg["body"] = "This is the response!"
+        # TODO Support for binary payloads
+        return_msg["body"] = resp_body
 
         await websocket.send(json.dumps(return_msg))
         print("< {}".format(json.dumps(return_msg)))
