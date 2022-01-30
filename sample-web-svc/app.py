4b91b882168ddfbf93d44aa53c8fe07f165c5f50
Hemanth <hemanth.aero@gmail.com>
2022-01-30 18:12:25 +0530

    Sample svc enhanced for GET calls

--- sample-web-svc/app.py
+++ sample-web-svc/app.py
@@ -5,9 +5,16 @@ from flask import request
 app = Flask (__name__)
 
 @app.route('/greetings',methods=['POST'])
-def hello():
+def greet():
   response = {
     'greeting': 'Hello ' + request.json['name']
+  }
+  return jsonify(response)
+
+@app.route('/greetings/<greetingId>',methods=['GET'])
+def retrieve(greetingId):
+  response = {
+    'greeting': 'Greeting #' + greetingId
   }
   return jsonify(response)
 
