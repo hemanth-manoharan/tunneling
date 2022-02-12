b056beb0abb95321ee2ffd9b7ab71d6b8e84dc86
Hemanth <hemanth.aero@gmail.com>
2022-02-12 20:02:36 +0530

    Formatting, docstrings for client

--- tunnel-client/client.py
+++ tunnel-client/client.py
@@ -1,61 +1,74 @@
 import asyncio
-import aiohttp
 import configparser
-import websockets
 import json
 import logging
 import base64
 import httputil
+import aiohttp
+import websockets
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 logging.basicConfig(level=config["general"]["LoggingLevel"])
 local_svc = config["local-service"]["ServiceURL"]
 
+
 async def tunnelling_client_loop():
-  async with websockets.connect(config["tunnelling-server"]["WebSocketURL"]) as websocket:
+    """
+    Indefinite loop for Web Socket Client
+    """
 
-    session = aiohttp.ClientSession()
+    async with websockets.connect(
+            config["tunnelling-server"]["WebSocketURL"]) as websocket:
 
-    while True:
-      try:
-        logging.debug("Waiting for next message ...")
-        req_msg = await websocket.recv()
-        logging.debug("> {}".format(req_msg))
+        session = aiohttp.ClientSession()
 
-        # Make the call using aiohttp
-        # https://docs.aiohttp.org/en/stable/client_quickstart.html
-        req_msg_json = json.loads(req_msg)
-        req_method = req_msg_json["method"]
-        if req_method == "GET" or req_method == "DELETE":
-          # TODO DELETE with body scenario not supported yet
-          async with session.request(req_method, 
-            local_svc + req_msg_json["uri"], headers=req_msg_json["headers"]) as resp:
-            resp_status, resp_headers, resp_body = await httputil.extract_response_details(resp, logging)
-        elif req_method == "POST" or req_method == "PUT":
-          async with session.request(req_method,
-            local_svc + req_msg_json["uri"],
-            data = base64.b64decode(req_msg_json["body"]), headers = req_msg_json["headers"]) as resp:
-            resp_status, resp_headers, resp_body = await httputil.extract_response_details(resp, logging)
-        else:
-          # TODO Handle this case better
-          logging.error("Unsupported request method. Ignoring ...")
-          continue
-        
-        # Construct new resp object here
-        return_msg = {
-          "id": req_msg_json["id"],
-          "status": resp_status,
-          "headers": resp_headers,
-          "body": resp_body
-        }
+        while True:
+            try:
+                logging.debug("Waiting for next message ...")
+                req_msg = await websocket.recv()
+                logging.debug("> %s", req_msg)
 
-        await websocket.send(json.dumps(return_msg))
+                # Make the call using aiohttp
+                # https://docs.aiohttp.org/en/stable/client_quickstart.html
+                req_msg_json = json.loads(req_msg)
+                req_method = req_msg_json["method"]
+                if req_method == "GET" or req_method == "DELETE":
+                    # TODO DELETE with body scenario not supported yet
+                    async with session.request(
+                            req_method,
+                            local_svc + req_msg_json["uri"],
+                            headers=req_msg_json["headers"]) as resp:
+                        resp_status, resp_headers, resp_body = await httputil.extract_resp_details(
+                            resp, logging)
+                elif req_method == "POST" or req_method == "PUT":
+                    async with session.request(
+                            req_method,
+                            local_svc + req_msg_json["uri"],
+                            data=base64.b64decode(req_msg_json["body"]),
+                            headers=req_msg_json["headers"]) as resp:
+                        resp_status, resp_headers, resp_body = await httputil.extract_resp_details(
+                            resp, logging)
+                else:
+                    # TODO Handle this case better
+                    logging.error("Unsupported request method. Ignoring ...")
+                    continue
 
-      except websockets.ConnectionClosed:
-        logging.info(f"Exception: WebSocket connection closed...")
-        break
+                # Construct new resp object here
+                return_msg = {
+                    "id": req_msg_json["id"],
+                    "status": resp_status,
+                    "headers": resp_headers,
+                    "body": resp_body
+                }
 
-    await session.close()
+                await websocket.send(json.dumps(return_msg))
+
+            except websockets.ConnectionClosed:
+                logging.info("Exception: WebSocket connection closed...")
+                break
+
+        await session.close()
+
 
 asyncio.get_event_loop().run_until_complete(tunnelling_client_loop())
--- tunnel-client/httputil.py
+++ tunnel-client/httputil.py
@@ -1,38 +1,50 @@
 import base64
 
+
 def _is_chunked(headers):
-  return headers.get("transfer-encoding") == "chunked"
+    return headers.get("transfer-encoding") == "chunked"
+
 
 def _get_headers_dict(headers):
-  normal_dict = {}
+    normal_dict = {}
 
-  # TODO Handle for duplicates
-  for key in headers.keys():
-    # Do not relay transfer-encoding header as it is a per hop header
-    if key.lower() == "transfer-encoding":
-      continue
-    # TODO Relaying the content-encoding header is causing issues.
-    # for javascript files. To be investigated.
-    if key.lower() == "content-encoding":
-      continue
-    normal_dict[key] = headers[key]
-  return normal_dict
+    # TODO Handle for duplicates
+    for key in headers.keys():
+        # Do not relay transfer-encoding header as it is a per hop header
+        if key.lower() == "transfer-encoding":
+            continue
+        # TODO Relaying the content-encoding header is causing issues.
+        # for javascript files. To be investigated.
+        if key.lower() == "content-encoding":
+            continue
+        normal_dict[key] = headers[key]
+    return normal_dict
 
-async def get_resp_body_str(httpResponse, logging):
-  if _is_chunked(httpResponse.headers):
-    buffer = b""
-    async for data, _ in httpResponse.content.iter_chunks():
-      buffer += data
-      resp_body = base64.b64encode(buffer).decode('utf-8')
-  else:
-    resp_body_bytes = await httpResponse.read()
-    resp_body = base64.b64encode(resp_body_bytes).decode('utf-8')
-  return resp_body
 
-async def extract_response_details(httpResponse, logging):
-  logging.info("Response status: " + str(httpResponse.status))
-  logging.debug("Response headers: " + str(httpResponse.headers))
-  resp_status = str(httpResponse.status)
-  resp_headers = _get_headers_dict(httpResponse.headers)
-  resp_body = await get_resp_body_str(httpResponse, logging)
-  return resp_status, resp_headers, resp_body
+async def get_resp_body_str(http_response, logging):
+    """
+    Extracts the http response body as a base64 encoded string
+    """
+
+    if _is_chunked(http_response.headers):
+        buffer = b""
+        async for data, _ in http_response.content.iter_chunks():
+            buffer += data
+            resp_body = base64.b64encode(buffer).decode('utf-8')
+    else:
+        resp_body_bytes = await http_response.read()
+        resp_body = base64.b64encode(resp_body_bytes).decode('utf-8')
+    return resp_body
+
+
+async def extract_resp_details(http_response, logging):
+    """
+    Extracts the http response header and body details
+    """
+
+    logging.info("Response status: %s", str(http_response.status))
+    logging.debug("Response headers: %s", str(http_response.headers))
+    resp_status = str(http_response.status)
+    resp_headers = _get_headers_dict(http_response.headers)
+    resp_body = await get_resp_body_str(http_response, logging)
+    return resp_status, resp_headers, resp_body
